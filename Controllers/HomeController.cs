5d0459e04ec411e0eb0f625b97015e2be7a3189d
NickToast <nicholastangsouvanh@gmail.com>
2023-08-04 00:50:15 -0700

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RazorFun.Models;
+
+namespace RazorFun.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
